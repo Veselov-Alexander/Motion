ea680f789021d272c00d511fc30f5a773d616432
Oleksandr Veselov <alexander.veselov.dev@gmail.com>
2024-10-19 02:51:49 +0300

    Remove omp include

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,8 @@
 #include "motion/app_window.h"
 #include <QtWidgets/QApplication>
-#include <omp.h>
 
 int main(int argc, char* argv[])
 {
-    omp_set_num_threads(8);
     srand(time(0));
     QApplication app(argc, argv);
     Motion::AppWindow window;
